8ffa04d98afc558c2a4ff67b0c71702c6023a69b
Lisa-Shima <shimalisa2@gmail.com>
2025-01-17 17:16:17 +0200

    Committing - Added authorization middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
+const protect = async(req, res, next) => {
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        try{
+            token = req.headers.authorization.split(' ')[1]
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            req.user = await User.find(decoded.id)
+            next()
+        }
+        catch(err){
+            res.status(401).json({success: false, message: 'Not authorized'})
+        }
+    }
+
+    if(!token) return res.status(401).json({success: false, message: 'No token provided'})
+}
+
+module.exports = {protect}
